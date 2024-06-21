ef47607f4776bb8fe98c8107d1b0bb85f4e1353a
IT Compliance <compliance@amalitech.org>
2024-06-21 08:28:14 -0700

    select time format

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// object representing current time 
-// const currentTime = new Date();
-// const hours = currentTime.getHours()
-// const minutes = currentTime.getMinutes()
-// const seconds = currentTime.getSeconds()
-// console.log(currentTime, hours, minutes, seconds)
-
+const clockEl = document.querySelector('#clock') 
+const radioEl = document.querySelectorAll('input[name="format"]')
 // create clock instances and access their properties
 const clockFactory = () => {
     const currentTime = new Date();
@@ -12,6 +7,7 @@ const clockFactory = () => {
         hours: currentTime.getHours(),
         minutes: currentTime.getMinutes(),
         seconds: currentTime.getSeconds(),
+
         getFormattedTime () {
             return `${ this.hours.toString().padStart(2, '0')}:${this.minutes.toString().padStart(2, '0')}:${this.seconds.toString().padStart(2, '0')}`
         }
@@ -21,10 +17,51 @@ const clockFactory = () => {
             hours = hours ? hours : 12
             const ampm = this.hours >= 12 ? 'PM' : 'AM'
             return `${hours.toString().padStart(2, '0')}:${this.minutes.toString().padStart(2, '0')}:${this.seconds.toString().padStart(2, '0')} ${ampm}`
+        },
+
+        // dynamically update clock
+        updateTime() {
+            const currentTime = new Date()
+            this.hours = currentTime.getHours()
+            this.minutes = currentTime.getMinutes()
+            this.seconds = currentTime.getSeconds()
         }
     }
 }
 
+
 const myClock = clockFactory()
-console.log(myClock.get12HourTime())
-console.log(myClock.getFormattedTime())
+
+// display clock function
+function displayClock(clock, isFormattedTime) {
+    let timeString
+    if(isFormattedTime){
+        timeString = clock.getFormattedTime()
+    }
+    else {
+       timeString = clock.get12HourTime()
+    }
+    clockEl.textContent = timeString 
+}
+
+
+// Update the clock display based on selected radio button
+function updateClockDisplay(clock) {
+    const selectedFormat = document.querySelector('input[name="format"]:checked').value;
+    const isFormattedTime = selectedFormat === '24'; // Check if 24-hour format is selected
+    displayClock(clock, isFormattedTime);
+}
+
+// Update the clock display every second
+updateClockDisplay(myClock)
+setInterval(() => {
+    myClock.updateTime()
+     updateClockDisplay(myClock)
+}, 1000);
+
+
+radioEl.forEach(input => {
+    input.addEventListener('change', () => {
+        updateClockDisplay(myClock);
+    });
+});
